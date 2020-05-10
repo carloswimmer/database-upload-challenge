3b85115d0ff9f0e64606a6bcb339c8239debd51e
Carlos Wimmer <coiote777@gmail.com>
2020-05-09 23:13:02 -0300

    Create transactions GET

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,8 +10,28 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<void> {
-    // TODO
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const totalIncome = transactions.reduce(
+      (total: number, transaction) =>
+        transaction.type === 'income' ? total + transaction.value : total,
+      0,
+    );
+
+    const totalOutcome = transactions.reduce(
+      (total: number, transaction) =>
+        transaction.type === 'outcome' ? total + transaction.value : total,
+      0,
+    );
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: totalIncome - totalOutcome,
+    };
+
+    return balance;
   }
 }
 
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,17 +1,24 @@
 import { Router } from 'express';
-// import { getCustomRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 
 // import Category from '../models/Category';
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
-// transactionsRouter.get('/', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.get('/', async (request, response) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  const result = { transactions, balance };
+
+  return response.json(result);
+});
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
