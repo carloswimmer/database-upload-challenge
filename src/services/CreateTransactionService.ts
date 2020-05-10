44fb28bd561d9438b8b8b74c077f50142ea2fe32
Carlos Wimmer <coiote777@gmail.com>
2020-05-09 21:59:41 -0300

    Create transactions POST

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,45 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateCategoryService from './CreateCategoryService';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+    const createCategory = new CreateCategoryService();
+
+    const checkCategoryExists = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    const { id } =
+      checkCategoryExists || (await createCategory.execute(category));
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: id,
+    });
+
+    return transaction;
   }
 }
 
